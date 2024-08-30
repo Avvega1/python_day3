51362598dac8364da4fd4a347bc98c218b50879a
Vega <avvega1@cps.k12.il.us>
2024-08-30 15:08:41 -0500

    first commit

---
+++ lesson2.py
@@ -0,0 +1,44 @@
+#create a print function that prints
+#out the string " Hello World"
+# print("Hello World")
+# ask the user for the day
+# day_of_week = input("What day of the week is it?")
+# print("Today is" ,day_of_week)
+# movies_this_week = input("What movies are you watching this week?")
+# print("I am watching" + movies_this_week + "this week")
+# mood = input("How are you feeling today?")
+# print(" I am feeling" + mood )
+
+# data types for variables in python
+# strings - text
+# name = "Adrian" # this is a string data type
+# whenever it is wrapped in quotes it is a string
+# integers - whole numbers
+# year = 2024 # this is an integer data type
+# # does not need to be wrapped in quotes
+# yearFourFromNow = 2028
+# Subtract = yearFourFromNow - year
+# print(Subtract)
+# # floats - decimal numbers
+# priceBigMac = 3.99 # this is a float data type
+# priceDoublePounder = 4.99
+# totalprice = priceBigMac + priceDoublePounder
+# print(totalprice)
+# needs to have decimal point
+# booleans - True or False
+# isRaining = False
+# print(isRaining)
+# groceries = ["apples","bananas", " carrots"]
+# print(groceries)
+MovieList = ["deadpool","Avengers","Bee Movie"]
+print(MovieList)
+RestaurantList = ["KFC", "Popeyes", "Harolds"]
+print(RestaurantList)
+AddressHarolds = ["1361 N Milwaukee Ave, Chicago, IL 60622"]
+print(AddressHarolds)
+AddressKFC = ["1144 S Western Ave, Chicago, IL 60612"]
+print(AddressKFC)
+AddressPopeyes = ["1555 W Division St, Chicago, IL 60642"]
+print(AddressPopeyes)
+TimeOfMovie = ["deapool - 7:30 P.M.", "Avengers - 8:15 P.M.", "Bee Movie - 10:00 P.M."]
+print(TimeOfMovie)
